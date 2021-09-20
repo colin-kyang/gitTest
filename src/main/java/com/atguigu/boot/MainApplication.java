4adec7156f7a5a8d6cd3c8e8d02aad593cee17f1
kyang <kyang@stu.ecnu.edu.cn>
2021-09-20 19:33:52 +0800

    first commit

---
+++ src/main/java/com/atguigu/boot/MainApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//主程序类
+//@SpringBootApplication:这是一个SpringBoot应用
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        //尝试使用Git版本管理
+        System.out.printf("hello github");
+        SpringApplication.run(MainApplication.class,args);
+    }
+
+}
