13d45c65616d10f59be0f4a6977ee9f402f6605a
kyang <kyang@stu.ecnu.edu.cn>
2021-09-20 20:11:31 +0800

    master 远程库测试

--- src/main/java/com/atguigu/boot/MainApplication.java
+++ src/main/java/com/atguigu/boot/MainApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApplication {
     public static void main(String[] args) {
         //尝试使用Git版本管理
-        System.out.println("开始运行SpringApplication");
+        System.out.println("远程库测试");
         SpringApplication.run(MainApplication.class,args);
     }
 }
